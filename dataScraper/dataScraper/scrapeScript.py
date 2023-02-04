1a032ae33a8ec9d5f1ff315a3a39cd9897d00a2e
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-02-04 14:02:22 +0100

    move dockerfile, requirements.txt to working dir 🤦‍♂️

--- dataScraper/dataScraper/scrapeScript.py
+++ dataScraper/dataScraper/scrapeScript.py
@@ -1,19 +1,13 @@
 from spiders.NationalCouncilMeeting_spider import NationalCouncilMeetingSpider
-from pipelines import MongoDBPipeline
-
-from pymongo import MongoClient
-
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 # https://victorsanner.nl/azure/scraping/container/instances/docker/2022/04/25/cheap-and-easy-scraping-using-scrapy-docker-and-azure-container-instances.html
 
-settings = get_project_settings()
-
 #this is where we start the scraper 
 def scrape():
-    process = CrawlerProcess(settings)
+    process = CrawlerProcess(get_project_settings())
     process.crawl(NationalCouncilMeetingSpider)
     process.start() # the script will block here until the crawling is finished
 
--- dataScraper/dataScraper/settings.py
+++ dataScraper/dataScraper/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'dataScraper.pipelines.MongoDBPipeline': 300,
+   "dataScraper.pipelines.MongoDBPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
