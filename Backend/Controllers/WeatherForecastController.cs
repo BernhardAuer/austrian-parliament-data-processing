0039b1c4d878f815bfe5803f2f460f118bc42e35
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-02-06 22:30:39 +0100

    add mongoDB connection to backend (first messy draft)

--- Backend/Controllers/WeatherForecastController.cs
+++ Backend/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using WebApi.Models;
 
 namespace WebApi.Controllers;
 
@@ -11,22 +14,25 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private readonly IMongoCollection<SpeechesMetaData> _speechesMetaDataCollection;
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptions<AustrianParliamentScrapeDatabaseSettings> austrianParliamentScrapeDatabaseSettings)
     {
         _logger = logger;
+        var mongoClient = new MongoClient(
+            austrianParliamentScrapeDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            austrianParliamentScrapeDatabaseSettings.Value.DatabaseName);
+
+        _speechesMetaDataCollection = mongoDatabase.GetCollection<SpeechesMetaData>(
+            austrianParliamentScrapeDatabaseSettings.Value.SpeechesMetaDataCollectionName);
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<List<SpeechesMetaData>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return await _speechesMetaDataCollection.Find(_ => true).ToListAsync();
     }
 }
