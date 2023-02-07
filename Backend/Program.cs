052b61ca3bade811bab4acd9cea4d9a2e4a83d0c
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-02-08 00:07:59 +0100

    enable CORS for local dev port 5173

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,6 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<AustrianParliamentScrapeDatabaseSettings>(
     builder.Configuration.GetSection("AustrianParliamentScrapeDatabase"));
 
+// for local dev only & no named policy, so we don't have mark controllers with the enableCors attribute!!
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins(
+                    "http://127.0.0.1:5173",
+                    "http://localhost:5173",
+                    "https://127.0.0.1:5173",
+                    "https://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +32,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
