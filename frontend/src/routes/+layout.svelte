77b2de7473e8dd84e29d9f2349c2ae75b5739728
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-18 21:37:48 +0200

    add footer and add links to navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,44 +1,50 @@
 <script>
 	import '../app.css';
+	import Footer from './footer.svelte'
 </script>
 
-<div class="navbar bg-base-100 shadow-sm">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<label tabindex="0" class="btn btn-ghost lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
+<div class="flex flex-col h-screen justify-between">
+	<div class="navbar bg-base-100 shadow-sm">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<label tabindex="0" class="btn btn-ghost lg:hidden">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h8m-8 6h16"
+						/></svg
+					>
+				</label>
+				<ul
+					tabindex="0"
+					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
 				>
-			</label>
-			<ul
-				tabindex="0"
-				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-			>
-				<li><a>Wortmeldungsarten</a></li>				
+					<li><a href="/">Wortmeldungsarten</a></li>				
+				</ul>
+			</div>
+			<a class="btn btn-ghost normal-case text-xl" href="/">parli-info.org</a>
+		</div>
+
+		<div class="navbar-center hidden lg:flex">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/">Wortmeldungsarten</a></li>			
 			</ul>
 		</div>
-		<a class="btn btn-ghost normal-case text-xl">parli Info</a>
+		<div class="navbar-end">
+		</div>
 	</div>
 
-	<div class="navbar-center hidden lg:flex">
-		<ul class="menu menu-horizontal px-1">
-			<li><a>Wortmeldungsarten</a></li>			
-		</ul>
+	<div class="mb-auto container sm:px-6 px-3 sm:min-w-fit min-w-full">
+		<slot />
 	</div>
-	<div class="navbar-end">
-	</div>
+	
+	<Footer class="self-end"></Footer>
 </div>
 
-<div class="container sm:px-6 px-3 sm:min-w-fit min-w-full">
-	<slot />
-</div>
---
+++ frontend/src/routes/footer.svelte
@@ -0,0 +1,6 @@
+<footer class="footer footer-center p-5 bg-base-200 text-base-content rounded">
+	<div class="grid grid-flow-col gap-4">
+	  <a class="link link-hover" href="/dataPrivacy">Datenschutz</a> 
+	  <a class="link link-hover" href="/impressum">Impressum</a>
+	</div> 
+</footer>
