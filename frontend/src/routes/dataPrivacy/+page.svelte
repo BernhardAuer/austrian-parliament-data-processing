1e4a02d1ded4ef17171564dce3169166b975fa4f
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-18 21:02:36 +0200

    add imprint and gpdr

---
+++ frontend/src/routes/dataPrivacy/+page.svelte
@@ -0,0 +1,60 @@
+
+<script>
+    import { env } from '$env/dynamic/public'
+    const name = env.PUBLIC_IMPRINT_NAME || '';
+    const contact = env.PUBLIC_IMPRINT_CONTACT || '';
+</script>
+
+<div class ="md:2xl:mx-96">
+    <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Erklärung zur Informationspflicht</h1>
+
+    <h2 class="text-2xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words">Datenschutzerklärung</h2>
+
+    In folgender Datenschutzerklärung informieren wir Sie über die wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Webseite. Wir erheben und verarbeiten personenbezogene Daten nur auf Grundlage der gesetzlichen Bestimmungen (Datenschutzgrundverordnung, Telekommunikationsgesetz 2003).
+
+    Sobald Sie als Benutzer auf unsere Webseite zugreifen oder diese besuchen wird Ihre IP-Adresse, Beginn und Ende der Sitzung erfasst. Dies ist technisch bedingt und stellt somit ein berechtigtes Interesse iSv Art 6 Abs 1 lit f DSGVO.
+
+    <h2 class="text-xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Kontakt mit uns</h2>
+
+    Wenn Sie uns per Email kontaktieren, dann werden die von Ihnen an uns übermittelten Daten zwecks Bearbeitung Ihrer Anfrage oder für den Fall von weiteren Anschlussfragen für sechs Monate bei uns gespeichert. Es erfolgt, ohne Ihre Einwilligung, keine Weitergabe Ihrer übermittelten Daten.
+
+    <h2 class="text-xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Cookies</h2>
+
+    Unsere Website verwendet so genannte Cookies. Dabei handelt es sich um kleine Textdateien, die mit Hilfe des Browsers auf Ihrem Endgerät abgelegt werden. Sie richten keinen Schaden an. Wir nutzen Cookies dazu, unser Angebot nutzerfreundlich zu gestalten. Einige Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Sie ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen. Wenn Sie dies nicht wünschen, so können Sie Ihren Browser so einrichten, dass er Sie über das Setzen von Cookies informiert und Sie dies nur im Einzelfall erlauben. Bei der Deaktivierung von Cookies kann die Funktionalität unserer Website eingeschränkt sein.
+
+    <h2 class="text-xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Server-Log Files</h2>
+
+    Diese Webseite und der damit verbundene Provider erhebt im Zuge der Webseitennutzung automatisch Informationen im Rahmen sogenannter „Server-Log Files“. Dies betrifft insbesondere:
+
+    -   IP-Adresse oder Hostname
+    -   den verwendeten Browser
+    -   Aufenthaltsdauer auf der Webseite sowie Datum und Uhrzeit
+    -   aufgerufene Seiten der Webseite
+    -   Spracheinstellungen und Betriebssystem
+    -   „Leaving-Page“ (auf welcher URL hat der Benutzer die Webseite verlassen)
+    -   ISP (Internet Service Provider)
+
+    Diese erhobenen Informationen werden nicht personenbezogen verarbeitet oder mit personenbezogenen Daten in Verbindung gebracht.
+
+    Der Webseitenbetreiber behält es sich vor, im Falle von Bekanntwerden rechtswidriger Tätigkeiten, diese Daten auszuwerten oder zu überprüfen.
+
+    <h2 class="text-xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Ihre Rechte als Betroffener</h2>
+
+    Sie als Betroffener haben bezüglich Ihrer Daten, welche bei uns gespeichert sind grundsätzlich ein Recht auf:
+    <ul class="list-disc list-inside">
+        <li>   Auskunft</li>
+        <li>   Löschung der Daten</li>
+        <li>   Berichtigung der Daten</li>
+        <li>   Übertragbarkeit der Daten</li>
+        <li>   Wiederruf und Widerspruch zur Datenverarbeitung</li>
+        <li>   Einschränkung</li>
+    </ul>
+    Wenn sie vermuten, dass im Zuge der Verarbeitung Ihrer Daten Verstöße gegen das Datenschutzrecht passiert sind, so haben Sie die Möglichkeit sich bei uns (auer-bernhard@pm.me) oder der Datenschutzbehörde zu beschweren.
+
+    <h2 class="text-xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Sie erreichen uns unter folgenden Kontaktdaten:</h2>
+
+    <h3 class="text-lg font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Webseitenbetreiber: {name}</h3> 
+    <h3 class="text-lg font-normal leading-normal mt-0 mb-2 text-blue-800 break-words"> Email: {contact} </h3> 
+
+    Quelle:  <a class="link" href="https://www.rechtstexte-generator.at/dsgvo-generator-oesterreich/">DSGVO Generator Österreich</a>  in Kooperation mit <a class="link" href="http://krankenversicherung123.at/">Private Krankenversicherung Österreich</a>
+</div>
--- frontend/src/routes/footer.svelte
+++ frontend/src/routes/footer.svelte
@@ -1,6 +1,6 @@
 <footer class="footer footer-center p-5 bg-base-200 text-base-content rounded">
 	<div class="grid grid-flow-col gap-4">
 	  <a class="link link-hover" href="/dataPrivacy">Datenschutz</a> 
-	  <a class="link link-hover" href="/impressum">Impressum</a>
+	  <a class="link link-hover" href="/imprint">Impressum</a>
 	</div> 
 </footer>
