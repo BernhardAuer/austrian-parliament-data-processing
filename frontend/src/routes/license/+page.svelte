d69dbe89c76b61354f9cdeedb7e578572765ec7e
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-19 18:17:11 +0200

    update footer and licensing

--- frontend/src/routes/license/+page.svelte
+++ frontend/src/routes/license/+page.svelte
@@ -9,10 +9,33 @@
     <br>
     Es wurden keine inhaltlichen Veränderungen an den Originalinhalten vorgenommen.
     <br>  <br>
+
     <p>
         Die verwendeten Inhalte werden von der österreichischen Parlamentsdirektion auf der Parlamentswebsite <a class="link" href="https://www.parlament.gv.at/">www.parlament.gv.at</a>
         zur Verfügung gestellt.  <a class="link" href="https://www.parlament.gv.at/recherchieren/open-data/daten-und-lizenz/index.html">Zum Open-Data Angebot des österreichischen Parlaments</a>
     </p> <br>
 
-    Der Lizenzvermerk gilt explizit nicht für den Quellcode dieser Webapplikation.
+    Der Lizenzvermerk gilt explizit nicht für den Quellcode dieser Webapplikation.<br> <br>
+
+    <h2 class="text-2xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words">Disclaimer</h2>
+    Die Parlamentsdirektion stellt auf der Parlamentswebsite im Rahmen von Open Data Rohdaten zur Verfügung. Daten, die Teil dieses Open-Data-Angebots sind, werden als solche ausgewiesen. Alles, was nicht als Open Data ausgewiesen oder beschrieben ist und trotzdem maschinenlesbar zur Verfügung steht, fällt nicht unter das Open-Data-Angebot der Parlamentsdirektion und unterliegt somit den allgemeinen Nutzungsbedingungen der Parlamentswebsite. 
+    
+    Die Nutzung des Open-Data-Angebots ist ausschließlich unter den Bedingungen der jeweils ausgewiesenen Lizenzen zulässig. Soweit dies durch eine solche Lizenz nicht geregelt ist, gelten die nachstehenden Nutzungsbedingungen und der untenstehende Haftungsausschluss.
+    
+    Bei Nutzungen des Open-Data-Angebots sind die gesetzlichen Bestimmungen, insbesondere zu Urheberrechten, Datenschutz und Persönlichkeitsrechten, einzuhalten. Sofern offensichtlich Rechte Dritter an Inhalten oder Daten bestehen, sind diese vor jeglicher Nutzung von dem:r Nutzer:in selbst zu klären. 
+    
+    Weiters behält sich die Parlamentsdirektion vor, das Open-Data-Angebot jederzeit einzustellen oder zu unterbrechen. Auch wenn sich die Parlamentsdirektion um eine Ankündigung oder rasche Behebung von Unterbrechungen bemüht, kann dies weder zugesichert noch gewährleistet werden.
+    
+    Schließlich behält sich die Parlamentsdirektion vor, Nutzungen zu untersagen, wenn dies zur Wahrung der Würde der parlamentarischen Körperschaften oder aus anderen berechtigten Gründen erfolgt. Im Fall der Untersagung ist jegliche Nutzung unverzüglich einzustellen, ansonsten bleiben weitere rechtliche Schritte vorbehalten.
+    
+    <h2 class="text-2xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words">Haftungsausschluss</h2>
+    Das Open-Data-Angebot wird freiwillig und unentgeltlich zur Verfügung gestellt.
+    Die Parlamentsdirektion übernimmt keinerlei Verantwortlichkeit und Haftung, auch nicht bei leichter Fahrlässigkeit, 
+    
+    für fehlerhafte Daten oder für deren Fehlinterpretation;
+    für die Beschreibungen der zur Verfügung gestellten Daten, insbesondere Abweichungen von diesen können vorkommen;
+    für die Richtigkeit, Vollständigkeit und Aktualität der Daten;
+    für Schäden oder Nachteile, welcher Art auch immer, die im Vertrauen auf diese oder durch deren Verwendung entstehen, sowie ebenso wenig
+    für mittels elektronischer Verweise zugängliche, fremde Informationen sowie für Schäden durch den Aufruf von elektronischen Verweisen in den Daten. 
+
 </div>
