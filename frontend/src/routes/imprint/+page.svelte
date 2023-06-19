2ac0a626d1f5f4367b4d1653a825d5b8d810e187
BernhardAuer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-19 18:25:13 +0200

    4 prepare 1 release (#5)
    * Update azure-static-web-apps-gray-bay-05ef8ee03.yml
    
    * use custom api instead of managed api for SWA
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Update main_aut-parl-api.yml
    
    * Delete 3-domain-kaufen-email-einrichten-yt-kanal-+-weitere-sm-kanäle-erstellen_aut-parl-api(int).yml
    
    * Update aut-parliamentary-data-scraping-AutoDeployTrigger-0ba95881-7efa-44d8-9919-0b393e06b5ad.yml
    
    * add custom favicon
    
    * only include custom daisy ui theme
    
    * add footer and add links to navbar
    
    * add imprint and gpdr
    
    * fix port in env file
    
    * add cc licensing data for content
    
    * update footer and licensing

---
+++ frontend/src/routes/imprint/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { env } from '$env/dynamic/public'
+    const name = env.PUBLIC_IMPRINT_NAME || '';
+    const contact = env.PUBLIC_IMPRINT_CONTACT || '';
+    const address = env.PUBLIC_IMPRINT_ADDRESS || '';
+</script>
+
+<div class="md:2xl:mx-96">
+    <h1 class="text-4xl font-normal leading-normal mt-0 mb-2 text-blue-800 break-words">Impressum</h1>
+    <p class="font-bold">Angaben gemäß § 25 Mediengesetz (Österreich) </p> <br>
+
+    Medieninhaber: {name} <br>
+    Kontakt: {contact} <br>
+    Adresse: {address} <br>
+
+    Diese Seite dient der einfachen Aufbereitung von Parlamentsdaten.
+</div>
