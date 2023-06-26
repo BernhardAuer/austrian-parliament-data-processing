83aee2f1e0c2f739611b8b38c462b254f3e18198
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-26 18:28:54 +0200

    finish skeletton layout for speech overview site

---
+++ frontend/src/routes/wortmeldungen/SpeechCard.svelte
@@ -0,0 +1,19 @@
+<div class="card min-w-fit max-w-xs bg-green-50 shadow-xl">
+    <div class="card-body"> 
+        <h2 class="card-title self-center">Pamela Rendi Wagner</h2>
+        <div class="relative flex items-center">
+            <div class="flex-grow border-t border-gray-400"></div>
+            <span class="flex-shrink mx-4 text-gray-400">SPÖ</span>
+            <div class="flex-grow border-t border-gray-400"></div>
+        </div> 
+       
+      <p class="text-justify"> [..] Eigentlich gehen wir oft und gerne davon aus, dass es sich beim Thema Tierschutz um eine Konsensmaterie handelt, dass der Tierschutz allen Parteien im Haus ein großes Anliegen ist und dass die Stimmen der Bevölkerung, die von der Politik mehr 
+        Tierwohl einfordern, wie die 427 000 Menschen, die gegen die quälenden Tiertransporte unterschrieben haben, gehört werden. [..]</p>
+
+        <div class="flex flex-wrap self-center gap-2">
+            <div class="badge badge-outline">PRO</div>
+            <div class="badge badge-outline">10:27 min</div>
+            <div class="badge badge-outline">1250 Wörter</div>           
+          </div>
+    </div>
+</div>
