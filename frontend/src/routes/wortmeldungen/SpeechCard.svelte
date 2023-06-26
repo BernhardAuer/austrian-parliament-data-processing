bc9a3568a3834f74c7f37c2d332228658cbbc041
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-26 23:05:52 +0200

    set background color according to speechType (achtung augenkrebs)

--- frontend/src/routes/wortmeldungen/SpeechCard.svelte
+++ frontend/src/routes/wortmeldungen/SpeechCard.svelte
@@ -1,8 +1,39 @@
 <script>
 	export let speech = null;
+
+  
+	const mapLabelsToBackgroundColor = (labelName) => {
+		labelName = labelName.toLowerCase();
+    // we neet to use the full string tailwind classes, so the preprocessor will pick this up correctly
+		let colorDict = {
+			"contra": "bg-[#F7464A]",
+			"pro": "bg-[#68E08C]",
+			"erste lesung": "bg-[#949FB1]",
+			"regierungsbank": "bg-[#4D5360]",
+			"tatsächliche berichtigung": "#bg-[FDB45C]",
+			"ap" : "bg-[#46BFBD]", 
+			"aktuelle stunde" : "bg-[#68bce0]",
+			"begründung" : "bg-[#e068bc]",
+			"berichterstattung ausschuss" : "bg-[#e08c68]", 
+			"dringliche anfrage" : "bg-[#e894c3]",
+			"dringlicher (entschließungs-)antrag" : "bg-[#b994e8]",
+			"erklärung" : "bg-[#94e8b9]", 
+			"erwiderung" : "bg-[#c3e894]",
+			"europäische union" : "#bg-[df6a71]",
+			"wortmeldung zur geschäftsbehandlung": "bg-[#e89499]",
+			"kurze debatte" : "bg-[#f6d3d5]",
+			"rf" : "bg-[#7f85e3]",  
+			"regierungsbank staatssekretär:in" : "bg-[#d3d5f6]",
+			"regierungsvorlage" : "bg-[#6adfd8]",
+			"unterzeichner:in" : "bg-[#d3f6f3]",
+			"wahldebatte" : "bg-[#df6a71]",
+			"wortmeldung" : "bg-[#f1bec1]",
+		}
+		return colorDict[labelName];
+	};
 </script>
 
-<div class="card min-w-fit max-w-xs bg-green-50 shadow-xl">
+<div class="card min-w-fit max-w-xs {mapLabelsToBackgroundColor(speech.typeOfSpeech)} shadow-xl">
     <div class="card-body"> 
         <h2 class="card-title self-center">{speech.nameOfSpeaker}</h2>
         <div class="relative flex items-center">
