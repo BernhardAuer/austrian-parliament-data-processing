4ebd6414481ee3db920f34856364f9a401731a95
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-06-26 22:02:57 +0200

    map shortNames of parties

--- frontend/src/routes/wortmeldungen/+page.svelte
+++ frontend/src/routes/wortmeldungen/+page.svelte
@@ -6,10 +6,21 @@
 	let speeches = [];
 	let speechesByTopic = [];
     let topics = [];
-
+    const getLongNameOfPoliticalParty = (shortName) => {
+        let mapDict = {
+			"v": "ÖVP",
+			"s": "SPÖ",
+			"f": "FPÖ",
+			"g": "GRÜNE",
+			"n": "NEOS",
+		}
+		let abbrToLower = shortName.toLowerCase();
+		return mapDict[abbrToLower];
+    }
 	const groupBy = (x, f) => x.reduce((a, b, i) => ((a[f(b, i, x)] ||= []).push(b), a), {}); // credit: https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
 	onMount(async () => {
 		speeches = await service.fetchSpeeches({ legislature: 'XXVII', meetingNumber: 217 });
+        speeches = speeches.map(x => {return {...x, politicalPartie: getLongNameOfPoliticalParty(x.politicalPartie)}});
 		speechesByTopic = groupBy(speeches, (x) => x.topic);
 
 		console.log(speechesByTopic);
