a0a2ca4c727913c3ab8000bc0daab9ecd806ef3b
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-07-02 00:52:58 +0200

    always open first accordion item

--- frontend/src/routes/wortmeldungen/[legislature]/[meetingNumber=integer]/+page.svelte
+++ frontend/src/routes/wortmeldungen/[legislature]/[meetingNumber=integer]/+page.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="py-4 space-y-2">
-	{#each data.topics as topic}
-		<TopicAccordion {topic} topNr={data.speechesByTopic[topic][0]?.topNr} speeches={data.speechesByTopic[topic]} />
+	{#each data.topics as topic, i}
+		<TopicAccordion {topic} topNr={data.speechesByTopic[topic][0]?.topNr} speeches={data.speechesByTopic[topic]} checked={i==0} />	
 	{/each}
 </div>
