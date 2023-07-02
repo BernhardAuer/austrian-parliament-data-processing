44ad409fc3925c02412cb8bfe976d711a8b42d65
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-07-02 23:01:08 +0200

    update node.js runtime to v18 for azure static web app

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,7 +4,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: azure()
+		adapter: azure({
+			customStaticWebAppConfig: {
+				"platform": {
+					"apiRuntime": "node:18"
+				}
+			}
+		})
 	},
 	preprocess: vitePreprocess()
 };
