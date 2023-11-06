a71d8aeb71fef3271ced6d218748e477971be14a
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-11-06 17:26:06 +0100

    add speech chat bubbles

--- frontend/src/routes/wortmeldung/[legislature]/[meetingNumber=integer]/[topic]/[speechNrInDebate=integer]/+page.svelte
+++ frontend/src/routes/wortmeldung/[legislature]/[meetingNumber=integer]/[topic]/[speechNrInDebate=integer]/+page.svelte
@@ -1,42 +1,36 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
 <svelte:head>
 	<title>Wortmeldung | parli-info.org</title>
 </svelte:head>
 
-<script>
-    /** @type {import('./$types').PageData} */
-    export let data;
-</script>
-
 <div class="py-4 space-y-2">
 	{#each data.speech as speech, i}
-		<!-- <TopicAccordion {topic} topNr={data.speechesByTopic[topic][0]?.topNr} speeches={data.speechesByTopic[topic]} checked={i==0} />	 -->
-	<!-- {topic.data} <br><br><br> -->
-
-	{#if speech.type == "info"}
-	<div class="flex justify-center items-center">
-
-		<div class="badge badge-accent">{speech.text}</div>
-	</div>
-	{:else}
-	<div class="chat chat-start">
-		<div class="chat-header">
-		  {speech.nameOfSpeaker ?? ""}
-		  {#if speech?.startTime !== undefined}			
-		  <time class="text-xs opacity-50">{speech.startTime}</time>
-		  {/if}
-		</div>
-		<div class="chat-bubble">
-		{#each speech?.text as text, j}
-				{text} 
-				<!-- do newlines only if there are further elements -->
-				{#if (j !== speech?.text?.length -1)}
-				<br> <br>
-				{/if}
-		{/each}
-	</div>	
-	  </div>
-{/if}
+		{#if speech.type == 'info'}
+			<div class="flex justify-center items-center">
+				<div class="badge badge-accent">{speech.text}</div>
+			</div>
+		{:else}
+			<div class="chat chat-start">
+				<div class="chat-header">
+					{speech.nameOfSpeaker ?? ''}
+					{#if speech?.startTime !== undefined}
+						<time class="text-xs opacity-50">{speech.startTime}</time>
+					{/if}
+				</div>
+				<div class="chat-bubble">
+					{#each speech?.text as text, j}
+						{text}
+						<!-- do newlines only if there are further elements -->
+						{#if j !== speech?.text?.length - 1}
+							<br /> <br />
+						{/if}
+					{/each}
+				</div>
+			</div>
+		{/if}
 	{/each}
-
-
 </div>
