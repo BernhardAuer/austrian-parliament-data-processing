8b2e773735ba68c8d03c5e37c3a0beab498b189e
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2023-11-07 18:51:13 +0100

    fix http 500

--- frontend/src/routes/wortmeldung/[legislature]/[meetingNumber=integer]/[topic]/[speechNrInDebate=integer]/+page.js
+++ frontend/src/routes/wortmeldung/[legislature]/[meetingNumber=integer]/[topic]/[speechNrInDebate=integer]/+page.js
@@ -1,8 +1,6 @@
 import { error } from '@sveltejs/kit';
 import SpeechService from './../../../../../../services/speechService.js';
 let service = new SpeechService();
-let speechesByTopic = [];
-let topics = [];
 export const csr = false;
 
 const groupBy = (x, f) => x.reduce((a, b, i) => ((a[f(b, i, x)] ||= []).push(b), a), {}); // credit: https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
@@ -11,12 +9,8 @@ const groupBy = (x, f) => x.reduce((a, b, i) => ((a[f(b, i, x)] ||= []).push(b),
 export async function load({ params }) {
     let speech = await service.fetchPureSpeech({ legislature: params.legislature, meetingNumber: params.meetingNumber, topic: params.topic,
     speechNrInDebate: params.speechNrInDebate });    
-    if (Array.isArray(speech) && !speech.length) {
+    if (speech == null) {
         throw error(404, 'Not found');
     }
-
-
-      
-    return { speech
-    };
+    return {speech};
 }
