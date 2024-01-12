7f862a2dfd9e195b8e75759bd09fc583231fe234
Bernhard Auer <40627756+BernhardAuer@users.noreply.github.com>
2024-01-12 23:14:39 +0100

    only insert new meetings

--- dataScraper/dataScraper/spiders/NationalCouncilMeeting_spider.py
+++ dataScraper/dataScraper/spiders/NationalCouncilMeeting_spider.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import scrapy
 import json
+from dataScraper.mongoProvider import MongoProvider
 from dataScraper.items import NationalCouncilMeetingItem
 from scrapy.loader import ItemLoader
 
@@ -9,10 +10,27 @@ class NationalCouncilMeetingSpider(scrapy.Spider):
     name = "nationalCouncilMeetings"
     search_url = "https://www.parlament.gv.at/Filter/api/json/post?jsMode=EVAL&FBEZ=WFP_007&listeId=11070&showAll=true&export=true"
     
-    def __init__(self, gp):
-        self.body = {"MODUS": ["PLENAR"], "NRBRBV": ["NR"], "GP": [], "R_SISTEI": ["SI"]}
-        self.body["GP"].append(gp)
+    def __init__(self, gp, mongodb_uri, mongodb_db):
+        self.body = {"MODUS": ["PLENAR"], "NRBRBV": ["NR"], "GP": [gp], "R_SISTEI": ["SI"]}
+        mongo_provider = MongoProvider(
+            mongodb_uri,
+            mongodb_db,
+            self.name
+        )        
+        collection = mongo_provider.get_collection()
+        latestItem = list(collection.find({"legislativePeriod": gp}).sort("meetingNumber", -1).limit(1))
+        self.nextMeetingNr = latestItem[0]["meetingNumber"] + 1 if len(latestItem) else 1
         
+    @classmethod
+    def from_crawler(cls, crawler, gp):
+        spider = cls(
+            gp,
+            mongodb_uri=crawler.settings.get('MONGODB_URI'),
+            mongodb_db=crawler.settings.get('MONGODB_DATABASE')
+        )
+        spider._set_crawler(crawler)
+        return spider 
+           
     def start_requests(self):
         return [
             scrapy.Request(
@@ -24,6 +42,9 @@ class NationalCouncilMeetingSpider(scrapy.Spider):
         results = json.loads(response.body)
 
         for meeting in results["rows"]:
+            if int(meeting[5]) < self.nextMeetingNr:
+                continue 
+            
             # todo: maybe use jmespath?
             l = ItemLoader(item=NationalCouncilMeetingItem(), selector=meeting)
             l.add_value("name", meeting[1])
